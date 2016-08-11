a466f5d45724e288b5255ffdf0e09d0645023526
Andrew Lintz <andrew.j.lintz@gmail.com>
2016-08-11 16:42:47 -0500

    nested the time day folder, splitting up the day into 4 folders: So far, now, todo/go, plan future.  Focusing on just day now.  Added an input form for Headline, story of the day.  Beginning to build the controlers. About to connect to the server.

---
+++ client/pages/03_time/01_day/02_now/nowdayPage.js
@@ -0,0 +1,27 @@
+Template.dayInput.events({
+	'submit #blogForm':function(e){
+		e.preventDefault();
+		var headline = $('#dayHeadline').val();
+		var story = $('#dayStory').val();
+		alert(headline + "" + story);
+	}
+});
+
+
+Template.daycounter.onCreated(function daycounterOnCreated() {
+  // counter starts at 0
+  this.counter = new ReactiveVar(10);
+});
+
+Template.daycounter.helpers({
+  counter() {
+    return Template.instance().counter.get();
+  },
+});
+
+Template.daycounter.events({
+  'click button'(event, instance) {
+    // increment the counter when button is clicked
+    instance.counter.set(instance.counter.get() + 1);
+  },
+});
