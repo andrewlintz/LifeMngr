2fe56107e5199daafacdceb17a30be2a1938ab1e
Andrew Lintz <andrew.j.lintz@gmail.com>
2016-08-12 15:40:15 -0500

    beginning to add collapsing menus.

--- client/pages/03_time/01_day/02_now/nowdayPage.js
+++ client/pages/03_time/01_day/02_now/nowdayPage.js
@@ -1,11 +1,6 @@
 
 
-
-
-
-
-
-Template.dayInput.events({
+Template.dayheadlineNstory.events({
 	'submit #blogForm':function(e){
 		e.preventDefault();
 		var headline = $('#dayHeadline').val();
@@ -19,20 +14,4 @@ Template.dayInput.events({
   }
 });
 
-Template.daycounter.onCreated(function daycounterOnCreated() {
-  // counter starts at 0
-  this.counter = new ReactiveVar(10);
-});
 
-Template.daycounter.helpers({
-  counter() {
-    return Template.instance().counter.get();
-  },
-});
-
-Template.daycounter.events({
-  'click button'(event, instance) {
-    // increment the counter when button is clicked
-    instance.counter.set(instance.counter.get() + 1);
-  },
-});
