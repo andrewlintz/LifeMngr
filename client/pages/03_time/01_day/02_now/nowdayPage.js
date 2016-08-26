f280e58f2643d3cfa5ba71e521cd01942a484a49
Andrew Lintz <andrew.j.lintz@gmail.com>
2016-08-26 13:02:57 -0500

    added companion: Pets & Garden, rearrange order

--- client/pages/03_time/01_day/02_now/nowdayPage.js
+++ client/pages/03_time/01_day/02_now/nowdayPage.js
@@ -1,4 +1,4 @@
-
+/*
 
 Template.dayheadlineNstory.events({
 	'submit #blogForm':function(e){
@@ -14,4 +14,19 @@ Template.dayheadlineNstory.events({
   }
 });
 
+*/
+
+Template.collapseCity.events({
+    'submit #todaysstory':function(e){
+        e.preventDefault();
+        var headline = $('#dayHeadline').val();
+        var story = $('#dayStory').val();
+    Daily.insert({
+        headline: headline,
+        story: story,
+        createdAt: new Date()
+    });
+    console.log("You have just added " + headline + " " + story + " successfully!");
+  }
+});
 
