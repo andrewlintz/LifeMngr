c3ecb9c9293d70f6e093431ab098d55470bc0c0f
Andrew Lintz <andrew.j.lintz@gmail.com>
2016-09-10 16:18:29 -0500

    demonstrated that we can now pass data through multiple pages via the router, up next: proliforate functions across entire app.

--- client/pages/06_mind/mindPage.js
+++ client/pages/06_mind/mindPage.js
@@ -1,36 +1,45 @@
-import './mind.html';
+import './mind.html'; 
 
-////////* Todos Events *//////
 
-Template.addtodosItem.events({
+///// HELPERS /////
+
+
+Template.mindPage.helpers({
+  agenda() {
+    return Agenda.find({}, {sort: {createdAt: -1}});  },
+});
+
+////////* Mood Events *//////
+
+Template.addmoodItem.events({
     /// events go here
     'submit form': function(event){
     event.preventDefault();
-    var todositemName = $('[name="todositemName"]').val();
+    var mooditemName = $('[name="mooditemName"]').val();
     Agenda.insert({
-        todos: todositemName,
+        mood: mooditemName,
         createdAt: new Date()
     });
-    $('[name="todositemName"]').val('');
+    $('[name="mooditemName"]').val('');
 }
 });
 
-Template.todosItem.events({
+Template.moodItem.events({
     // events go here
-    'click .delete-todositem': function(event){
+    'click .delete-mooditem': function(event){
     event.preventDefault();
     var documentId = this._id;
     var confirm = 
             Agenda.remove({ _id: documentId });
     },
 
-    'keyup [name=todosItem]': function(event){
+    'keyup [name=moodItem]': function(event){
     if(event.which == 13 || event.which == 27){
         $(event.target).blur();
     } else {
         var documentId = this._id;
-        var todosItem = $(event.target).val();
-        Agenda.update({ _id: documentId }, {$set: { todos: todosItem }});
+        var moodItem = $(event.target).val();
+        Agenda.update({ _id: documentId }, {$set: { mood: moodItem }});
         }
     },
 
