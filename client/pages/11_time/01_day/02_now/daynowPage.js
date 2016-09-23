e7fbf1243588b28287e91189afd394fd8d797ae5
Andrew Lintz <andrew.j.lintz@gmail.com>
2016-09-23 17:28:46 -0500

    refactored layout splitting regions of a page view accordingly, added Crud to day view now & review

--- client/pages/11_time/01_day/02_now/daynowPage.js
+++ client/pages/11_time/01_day/02_now/daynowPage.js
@@ -6,6 +6,10 @@
 
 ///// EVENTS /////
 
+Template.tempName.onRendered(function() {
+    this.$('.datetimepicker').datetimepicker();
+});
+
 // Life Journal events//
 Template.daysStory.events({
     'submit #todaysstory2':function(e){
@@ -39,4 +43,127 @@ Template.sleepLogger.events({
             createdAt: new Date()
         });
    },
+});
+//Waking events//
+
+Template.wakeLogger.events({
+    'submit #dnwakeupSubmitter':function(e){
+        e.preventDefault();
+        var wakeuptime = $('#dnwakeuptime').val();
+        Agenda.insert({
+            wakeuptime: wakeuptime,
+            createdAt: new Date()
+        });
+    },
+    'submit #dnoutofbedSubmitter':function(e){
+        e.preventDefault();
+        var outofBed = $('#dnoutofBed').val();
+        Agenda.insert({
+            outofBed: outofBed,
+            createdAt: new Date()
+        });
+    },
+});
+
+Template.mindLogger.events({
+    'submit #dnmoodSubmitter':function(e){
+        e.preventDefault();
+        var mood = $('#dnmood').val();
+        Agenda.insert({
+            mood: mood,
+            createdAt: new Date()
+        });
+    },
+    'submit #dnwhymoodSubmitter':function(e){
+        e.preventDefault();
+        var whymood = $('#dnwhymood').val();
+        Agenda.insert({
+            whymood: whymood,
+            createdAt: new Date()
+        });
+    },
+    'submit #dngoodhabbitsSubmitter':function(e){
+        e.preventDefault();
+        var goodhabbits = $('#dngoodhabbits').val();
+        Agenda.insert({
+            goodhabbits: goodhabbits,
+            createdAt: new Date()
+        });
+    },
+    'submit #dnbadhabbitsSubmitter':function(e){
+        e.preventDefault();
+        var badhabbits = $('#dnbadhabbits').val();
+        Agenda.insert({
+            badhabbits: badhabbits,
+            createdAt: new Date()
+        });
+    },
+    'submit #dnjokesSubmitter':function(e){
+        e.preventDefault();
+        var jokes = $('#dnjokes').val();
+        Agenda.insert({
+            jokes: jokes,
+            createdAt: new Date()
+        });
+    },
+    'submit #dncreativeartideasSubmitter':function(e){
+        e.preventDefault();
+        var creativeartideas = $('#dncreativeartideas').val();
+        Agenda.insert({
+            creativeartideas: creativeartideas,
+            createdAt: new Date()
+        });
+    },
+    'submit #dninventionsSubmitter':function(e){
+        e.preventDefault();
+        var inventions = $('#dninventions').val();
+        Agenda.insert({
+            inventions: inventions,
+            createdAt: new Date()
+        });
+    },
+    'submit #dnentreprenuerialideaSubmitter':function(e){
+        e.preventDefault();
+        var entreprenuerialidea = $('#dnentreprenuerialidea').val();
+        Agenda.insert({
+            entreprenuerialidea: entreprenuerialidea,
+            createdAt: new Date()
+        });
+    },
+    'submit #dnjobcareerbizOppSubmitter':function(e){
+        e.preventDefault();
+        var jobcareerbizOpp = $('#dnjobcareerbizOpp').val();
+        Agenda.insert({
+            jobcareerbizOpp: jobcareerbizOpp,
+            createdAt: new Date()
+        });
+    },
+    'submit #dnoppinionSubmitter':function(e){
+        e.preventDefault();
+        var oppinion = $('#dnoppinion').val();
+        Agenda.insert({
+            oppinion: oppinion,
+            createdAt: new Date()
+        });
+    },
+    'submit #dnwisdomSubmitter':function(e){
+        e.preventDefault();
+        var wisdom = $('#dnwisdom').val();
+        Agenda.insert({
+            wisdom: wisdom,
+            createdAt: new Date()
+        });
+    },
+});
+
+Template.agendaLogger.events({
+    'submit #dnmissionSubmitter':function(e){
+        e.preventDefault();
+        var mission = $('#dnmission').val();
+        Agenda.insert({
+            mission: mission,
+            createdAt: new Date()
+        });
+    },
+
 });
--- client/pages/11_time/01_day/03_review/dayreviewPage.js
+++ client/pages/11_time/01_day/03_review/dayreviewPage.js
@@ -28,7 +28,107 @@ Template.drLooper.helpers({
         end.setHours(23,59,59,999);
         return Agenda.find( {dreamDiary: {$exists: true}}, {createdAt: {$gte: start, $lt: end}}, {sort: {createdAt: -1}});
     },
+    drwakeUplooper() {
+        var start = new Date();
+        start.setHours(0,0,0,0);
+        var end = new Date();
+        end.setHours(23,59,59,999);
+        return Agenda.find( {wakeuptime: {$exists: true}}, {createdAt: {$gte: start, $lt: end}}, {sort: {createdAt: -1}});
+    },
+    droutofBedlooper() {
+        var start = new Date();
+        start.setHours(0,0,0,0);
+        var end = new Date();
+        end.setHours(23,59,59,999);
+        return Agenda.find( {outofBed: {$exists: true}}, {createdAt: {$gte: start, $lt: end}}, {sort: {createdAt: -1}});
+    },
+    drmoodlooper() {
+        var start = new Date();
+        start.setHours(0,0,0,0);
+        var end = new Date();
+        end.setHours(23,59,59,999);
+        return Agenda.find( {mood: {$exists: true}}, {createdAt: {$gte: start, $lt: end}}, {sort: {createdAt: -1}});
+    },
+    drwhymoodlooper() {
+        var start = new Date();
+        start.setHours(0,0,0,0);
+        var end = new Date();
+        end.setHours(23,59,59,999);
+        return Agenda.find( {whymood: {$exists: true}}, {createdAt: {$gte: start, $lt: end}}, {sort: {createdAt: -1}});
+    },
+    drgoodhabbitslooper() {
+        var start = new Date();
+        start.setHours(0,0,0,0);
+        var end = new Date();
+        end.setHours(23,59,59,999);
+        return Agenda.find( {goodhabbits: {$exists: true}}, {createdAt: {$gte: start, $lt: end}}, {sort: {createdAt: -1}});
+    },
+    drbadhabbitslooper() {
+        var start = new Date();
+        start.setHours(0,0,0,0);
+        var end = new Date();
+        end.setHours(23,59,59,999);
+        return Agenda.find( {badhabbits: {$exists: true}}, {createdAt: {$gte: start, $lt: end}}, {sort: {createdAt: -1}});
+    },
+    drjokeslooper() {
+        var start = new Date();
+        start.setHours(0,0,0,0);
+        var end = new Date();
+        end.setHours(23,59,59,999);
+        return Agenda.find( {jokes: {$exists: true}}, {createdAt: {$gte: start, $lt: end}}, {sort: {createdAt: -1}});
+    },
+    drcreativeartideaslooper() {
+        var start = new Date();
+        start.setHours(0,0,0,0);
+        var end = new Date();
+        end.setHours(23,59,59,999);
+        return Agenda.find( {creativeartideas: {$exists: true}}, {createdAt: {$gte: start, $lt: end}}, {sort: {createdAt: -1}});
+    },
+    drinventionslooper() {
+        var start = new Date();
+        start.setHours(0,0,0,0);
+        var end = new Date();
+        end.setHours(23,59,59,999);
+        return Agenda.find( {inventions: {$exists: true}}, {createdAt: {$gte: start, $lt: end}}, {sort: {createdAt: -1}});
+    },
+    drentreprenuerialidealooper() {
+        var start = new Date();
+        start.setHours(0,0,0,0);
+        var end = new Date();
+        end.setHours(23,59,59,999);
+        return Agenda.find( {entreprenuerialidea: {$exists: true}}, {createdAt: {$gte: start, $lt: end}}, {sort: {createdAt: -1}});
+    },
+    drjobcareerbizOpplooper() {
+        var start = new Date();
+        start.setHours(0,0,0,0);
+        var end = new Date();
+        end.setHours(23,59,59,999);
+        return Agenda.find( {jobcareerbizOpp: {$exists: true}}, {createdAt: {$gte: start, $lt: end}}, {sort: {createdAt: -1}});
+    },
+    droppinionlooper() {
+        var start = new Date();
+        start.setHours(0,0,0,0);
+        var end = new Date();
+        end.setHours(23,59,59,999);
+        return Agenda.find( {oppinion: {$exists: true}}, {createdAt: {$gte: start, $lt: end}}, {sort: {createdAt: -1}});
+    },
+    drwisdomlooper() {
+        var start = new Date();
+        start.setHours(0,0,0,0);
+        var end = new Date();
+        end.setHours(23,59,59,999);
+        return Agenda.find( {wisdom: {$exists: true}}, {createdAt: {$gte: start, $lt: end}}, {sort: {createdAt: -1}});
+    },
+    drmissionlooper() {
+        var start = new Date();
+        start.setHours(0,0,0,0);
+        var end = new Date();
+        end.setHours(23,59,59,999);
+        return Agenda.find( {mission: {$exists: true}}, {createdAt: {$gte: start, $lt: end}}, {sort: {createdAt: -1}});
+    },
+    
 });
+
 
 
 
@@ -55,7 +155,6 @@ Template.drheadliner.events({
         Agenda.update({ _id: documentId }, {$set: { headline: drheadlinerItem }});
         }
     },
-
 });
 
 Template.drstory.events({
@@ -76,7 +175,6 @@ Template.drstory.events({
         Agenda.update({ _id: documentId }, {$set: { story: dayreviewstoryItem }});
         }
     },
-
 });
 
 Template.drhowdidUsleep.events({
@@ -97,7 +195,6 @@ Template.drhowdidUsleep.events({
         Agenda.update({ _id: documentId }, {$set: { howdidUsleep: drhowdidUsleepItem }});
         }
     },
-
 });
 Template.drdreamDiaryloader.events({
     // events go here
@@ -116,6 +213,288 @@ Template.drdreamDiaryloader.events({
         var drdreamDiaryItem = $(event.target).val();
         Agenda.update({ _id: documentId }, {$set: { dreamDiary: drdreamDiaryItem }});
         }
+    },    
+});
+
+Template.drwakeUploader.events({
+    // events go here
+    'click .delete-drwakeUpitem': function(event){
+    event.preventDefault();
+    var documentId = this._id;
+    var confirm = 
+            Agenda.remove({ _id: documentId });
     },
 
+    'keyup [name=drwakeUpItem]': function(event){
+    if(event.which == 13 || event.which == 27){
+        $(event.target).blur();
+    } else {
+        var documentId = this._id;
+        var drwakeUpItem = $(event.target).val();
+        Agenda.update({ _id: documentId }, {$set: { wakeuptime: drwakeUpItem }});
+        }
+    },
+});
+
+Template.droutofBedloader.events({
+    // events go here
+    'click .delete-droutofBeditem': function(event){
+    event.preventDefault();
+    var documentId = this._id;
+    var confirm = 
+            Agenda.remove({ _id: documentId });
+    },
+
+    'keyup [name=droutofBedItem]': function(event){
+    if(event.which == 13 || event.which == 27){
+        $(event.target).blur();
+    } else {
+        var documentId = this._id;
+        var droutofBedItem = $(event.target).val();
+        Agenda.update({ _id: documentId }, {$set: { outofBed: droutofBedItem }});
+        }
+    },
+});
+
+Template.drmoodloader.events({
+    // events go here
+    'click .delete-drmooditem': function(event){
+    event.preventDefault();
+    var documentId = this._id;
+    var confirm = 
+            Agenda.remove({ _id: documentId });
+    },
+
+    'keyup [name=drmoodItem]': function(event){
+    if(event.which == 13 || event.which == 27){
+        $(event.target).blur();
+    } else {
+        var documentId = this._id;
+        var drmoodItem = $(event.target).val();
+        Agenda.update({ _id: documentId }, {$set: { mood: drmoodItem }});
+        }
+    },
+});
+
+Template.drwhymoodloader.events({
+    // events go here
+    'click .delete-drwhymooditem': function(event){
+    event.preventDefault();
+    var documentId = this._id;
+    var confirm = 
+            Agenda.remove({ _id: documentId });
+    },
+
+    'keyup [name=drwhymoodItem]': function(event){
+    if(event.which == 13 || event.which == 27){
+        $(event.target).blur();
+    } else {
+        var documentId = this._id;
+        var drwhymoodItem = $(event.target).val();
+        Agenda.update({ _id: documentId }, {$set: { whymood: drwhymoodItem }});
+        }
+    },
+});
+
+Template.drgoodhabbitsloader.events({
+    // events go here
+    'click .delete-drgoodhabbitsitem': function(event){
+    event.preventDefault();
+    var documentId = this._id;
+    var confirm = 
+            Agenda.remove({ _id: documentId });
+    },
+
+    'keyup [name=drgoodhabbitsItem]': function(event){
+    if(event.which == 13 || event.which == 27){
+        $(event.target).blur();
+    } else {
+        var documentId = this._id;
+        var drgoodhabbitsItem = $(event.target).val();
+        Agenda.update({ _id: documentId }, {$set: { goodhabbits: drgoodhabbitsItem }});
+        }
+    },
+});
+
+Template.drbadhabbitsloader.events({
+    // events go here
+    'click .delete-drbadhabbitsitem': function(event){
+    event.preventDefault();
+    var documentId = this._id;
+    var confirm = 
+            Agenda.remove({ _id: documentId });
+    },
+
+    'keyup [name=drbadhabbitsItem]': function(event){
+    if(event.which == 13 || event.which == 27){
+        $(event.target).blur();
+    } else {
+        var documentId = this._id;
+        var drbadhabbitsItem = $(event.target).val();
+        Agenda.update({ _id: documentId }, {$set: { badhabbits: drbadhabbitsItem }});
+        }
+    },
+});
+Template.drjokesloader.events({
+    // events go here
+    'click .delete-drjokesitem': function(event){
+    event.preventDefault();
+    var documentId = this._id;
+    var confirm = 
+            Agenda.remove({ _id: documentId });
+    },
+
+    'keyup [name=drjokesItem]': function(event){
+    if(event.which == 13 || event.which == 27){
+        $(event.target).blur();
+    } else {
+        var documentId = this._id;
+        var drjokesItem = $(event.target).val();
+        Agenda.update({ _id: documentId }, {$set: { jokes: drjokesItem }});
+        }
+    },
+});
+Template.drcreativeartideasloader.events({
+    // events go here
+    'click .delete-drcreativeartideasitem': function(event){
+    event.preventDefault();
+    var documentId = this._id;
+    var confirm = 
+            Agenda.remove({ _id: documentId });
+    },
+
+    'keyup [name=drcreativeartideasItem]': function(event){
+    if(event.which == 13 || event.which == 27){
+        $(event.target).blur();
+    } else {
+        var documentId = this._id;
+        var drcreativeartideasItem = $(event.target).val();
+        Agenda.update({ _id: documentId }, {$set: { creativeartideas: drcreativeartideasItem }});
+        }
+    },
+});
+Template.drinventionsloader.events({
+    // events go here
+    'click .delete-drinventionsitem': function(event){
+    event.preventDefault();
+    var documentId = this._id;
+    var confirm = 
+            Agenda.remove({ _id: documentId });
+    },
+
+    'keyup [name=drinventionsItem]': function(event){
+    if(event.which == 13 || event.which == 27){
+        $(event.target).blur();
+    } else {
+        var documentId = this._id;
+        var drinventionsItem = $(event.target).val();
+        Agenda.update({ _id: documentId }, {$set: { inventions: drinventionsItem }});
+        }
+    },
+});
+Template.drentreprenuerialidealoader.events({
+    // events go here
+    'click .delete-drentreprenuerialideaitem': function(event){
+    event.preventDefault();
+    var documentId = this._id;
+    var confirm = 
+            Agenda.remove({ _id: documentId });
+    },
+
+    'keyup [name=drentreprenuerialideaItem]': function(event){
+    if(event.which == 13 || event.which == 27){
+        $(event.target).blur();
+    } else {
+        var documentId = this._id;
+        var drentreprenuerialideaItem = $(event.target).val();
+        Agenda.update({ _id: documentId }, {$set: { entreprenuerialidea: drentreprenuerialideaItem }});
+        }
+    },
+});
+Template.drjobcareerbizOpploader.events({
+    // events go here
+    'click .delete-drjobcareerbizOppitem': function(event){
+    event.preventDefault();
+    var documentId = this._id;
+    var confirm = 
+            Agenda.remove({ _id: documentId });
+    },
+
+    'keyup [name=drjobcareerbizOppItem]': function(event){
+    if(event.which == 13 || event.which == 27){
+        $(event.target).blur();
+    } else {
+        var documentId = this._id;
+        var drjobcareerbizOppItem = $(event.target).val();
+        Agenda.update({ _id: documentId }, {$set: { jobcareerbizOpp: drjobcareerbizOppItem }});
+        }
+    },
+});
+Template.droppinionloader.events({
+    // events go here
+    'click .delete-droppinionitem': function(event){
+    event.preventDefault();
+    var documentId = this._id;
+    var confirm = 
+            Agenda.remove({ _id: documentId });
+    },
+
+    'keyup [name=droppinionItem]': function(event){
+    if(event.which == 13 || event.which == 27){
+        $(event.target).blur();
+    } else {
+        var documentId = this._id;
+        var droppinionItem = $(event.target).val();
+        Agenda.update({ _id: documentId }, {$set: { oppinion: droppinionItem }});
+        }
+    },
+});
+Template.drwisdomloader.events({
+    // events go here
+    'click .delete-drwisdomitem': function(event){
+    event.preventDefault();
+    var documentId = this._id;
+    var confirm = 
+            Agenda.remove({ _id: documentId });
+    },
+
+    'keyup [name=drwisdomItem]': function(event){
+    if(event.which == 13 || event.which == 27){
+        $(event.target).blur();
+    } else {
+        var documentId = this._id;
+        var drwisdomItem = $(event.target).val();
+        Agenda.update({ _id: documentId }, {$set: { wisdom: drwisdomItem }});
+        }
+    },
+});
+
+
+
+
+
+// AGENDA//
+
+
+
+
+
+Template.drmissionloader.events({
+    // events go here
+    'click .delete-drmissionitem': function(event){
+    event.preventDefault();
+    var documentId = this._id;
+    var confirm = 
+            Agenda.remove({ _id: documentId });
+    },
+
+    'keyup [name=drmissionItem]': function(event){
+    if(event.which == 13 || event.which == 27){
+        $(event.target).blur();
+    } else {
+        var documentId = this._id;
+        var drmissionItem = $(event.target).val();
+        Agenda.update({ _id: documentId }, {$set: { mission: drmissionItem }});
+        }
+    },
 });
