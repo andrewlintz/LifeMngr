a59c2676dc246a30b76895cd8c69f955c894cc8a
ATXcloud <Andrew.j.lintz@gmail.com>
2017-10-25 23:29:11 -0500

    solved create allDay record by Fullcalendar select

--- client/pages/01_chronos/calPage.js
+++ client/pages/01_chronos/calPage.js
@@ -2,6 +2,12 @@ import { DevTest } from '../../../lib/collections/collections.js';
 
 import './cal.html';
 
+
+
+//////////////////////////////////////////////////////
+// BELOW Manages Data Displayed onto the  CALENDAR  //
+//////////////////////////////////////////////////////
+
 Template.calPage.onRendered(function(){
     var fc = this.$('.fc');
     this.autorun(function () {
@@ -12,18 +18,6 @@ Template.calPage.onRendered(function(){
     });
 });
 
-////////////////////////////////
-// BELOW IS THE CALENDAR VIEW //
-////////////////////////////////
-
-Template.body.rendered = function () {
-    var fc = this.$('.fc');
-    this.autorun(function () {
-        DevTest.find();
-        fc.fullCalendar('refetchEvents');
-    });
-};
-
 ////////////////////////////////////////
 // BELOW IS THE CALENDAR VIEW HELPERS //
 ////////////////////////////////////////
@@ -33,7 +27,7 @@ Template.calendarView.helpers({
         var fc = $('.fc');
         return function (title, start, end, tz, callback) {
             //subscribe only to specified date range
-            Meteor.subscribe('events', title, start, end, function () {
+            Meteor.subscribe('events', title, start, end, allDay, function () {
                 //trigger event rendering when collection is downloaded
                 fc.fullCalendar('refetchEvents');
             });
@@ -60,7 +54,7 @@ Template.calendarView.helpers({
             lang: 'en',
             allDaySlot: true,
             header: {
-                left: 'prev,next today',
+                left: 'prev,today,next',
                 center: 'title',
                 right: 'month,agendaWeek,agendaDay'
             },
@@ -71,12 +65,17 @@ Template.calendarView.helpers({
                 month: 'h:mm a',
                 agendaWeek: 'h:mm a'
             },
+            weekNumbers: true,
+
+        // ScrollTime: Determines how far down the scroll pane is initially scrolled down
             scrollTime: '12:00:00',
             theme: false,
             editable: true,
             eventLimit: true,
+        // SlotDuration is the resolution of the calendar view.   
             slotDuration: '00:15:00',
             forceEventDuration: true, 
+        //  defaultTimedEventDuration sets the default length of event time added.
             defaultTimedEventDuration: '00:15:00', 
         //START from https://usingfullcalendar.wordpress.com/2016/03/31/creating-a-fullcalendar-event-interactively/
 
@@ -86,13 +85,30 @@ Template.calendarView.helpers({
 
        	// This is the callback that will be triggered when a selection is made.
         // It gets start and end date/time as part of its arguments
-       		select: function(start, end, jsEvent, view) {
+       		select: function(start, end, event, allDay, jsEvent, view) {
        	// Ties Click time/date to the DateTimePicker Input Field	
        	// Specifically helped by http://jsfiddle.net/mccannf/AzmJv/16/	
-       			
-          			$('#createEvent #startTime').val(start);
-          			$('#createEvent #endTime').val(end);
-        			$("#createEvent").modal("show");
+        
+        // Below is the Trick that correctly return whether or not user selects allDay or specific time on the calendar
+        // Answer sourced from here: https://stackoverflow.com/questions/29780273/fullcalendar-select-check-if-allday
+                var allDay2 = !start.hasTime() && !end.hasTime();
+                    console.log("This is " + allDay2)
+
+        /* switch string to boolean : https://stackoverflow.com/questions/14757994/jquery-fullcalendar-change-allday-criteria-to-check-for-string-instead-of-bool
+                if(event.allDay === true){
+                    console.log("true");
+                }
+                else{
+                    console.log("false");
+                }
+        */
+
+                    $('#createEvent #startTime').val(start);
+                    $('#createEvent #endTime').val(end);
+                    $('#createEvent #apptAllDay').val(allDay2);
+                    $("#createEvent").modal("show");
+                    //console.log($('#apptAllDay').val());
+                    //console.log(allDay);
         		
     		},
             events: function (start, end, timezone, callback) {
@@ -108,10 +124,11 @@ Template.calendarView.helpers({
             	}
     		},           
 
-            eventClick: function(title, start, end, jsEvent, element) {    
+            eventClick: function(title, start, end, allDay, jsEvent, element) {    
                 $('#eventTitle').val(title.title);
                 $('#startTime').val(title.start);
                 $('#endTime').val(title.end); 
+
                 $('#eventDescription').val(title.description); 
                 $('.updateModal').modal('show');
                 $('#calendar').fullCalendar('updateEvent', event);
@@ -136,10 +153,12 @@ Template.calPage.events({
         var title = event.target.title.value;
         var start = event.target.start.value;
         var end = event.target.end.value;
+// convert string to boolean https://stackoverflow.com/questions/263965/how-can-i-convert-a-string-to-boolean-in-javascript
+        var allDay3 = (event.target.apptAllDay.value == 'true');
         var description = event.target.description.value;
 
         // Insert a spirit item into the collection
-        Meteor.call('addEvent.insert', title, start, end, description);
+        Meteor.call('addEvent.insert', title, start, end, allDay3, description);
         
         // Clear form & closes modal
         event.target.title.value = "";
